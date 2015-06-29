a82162b3ad3b745b75fe35770a48a231bdc61fea
gholpl <license@olpl.org>
2015-06-29 10:29:54 -0500

    Initial Commit

---
+++ OLPL_App_MoveEvancedPictures/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OLPL_App_MoveEvancedPictures
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Settings st1 = new Settings();
+            st1 = Settings_Functions.getSettings();
+            foreach(string fl in Directory.GetFiles(st1.folder_source))
+            {
+                string filename = fl.Replace(st1.folder_source + '\\', "");
+                if (File.Exists(st1.folder_target + "\\" + filename))
+                {
+                    
+                    if (File.GetCreationTime(fl)>File.GetCreationTime(st1.folder_target + "\\" + filename))
+                    {
+                        File.Delete(st1.folder_target + "\\" + filename);
+                        File.Move(fl, st1.folder_target + "\\" + filename);
+                        Console.WriteLine("New version of the File Moved" + Environment.NewLine);
+                    }
+                    else { File.Delete(fl); }
+                }
+                else
+                {
+
+                    File.Move(fl, st1.folder_target + "\\" + filename);
+                    Console.WriteLine("New File Moved" + Environment.NewLine);
+                }
+                
+            }
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ OLPL_App_MoveEvancedPictures/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLPL_App_MoveEvancedPictures
+{
+    public class Settings
+    {
+        public string folder_source { get; set; }
+        public string folder_target { get; set; }
+    }
+    public class Settings_Functions
+    {
+        public static Settings getSettings()
+        {
+            Settings st1 = new Settings();
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string[] SettingMain = File.ReadAllLines(Path.GetDirectoryName(path) + "\\settings.ini");
+            st1.folder_source = getSettingINI(SettingMain, "folder.source");
+            st1.folder_target = getSettingINI(SettingMain, "folder.destination");
+            return st1;
+        }
+        public static string getSettingINI(string[] str, string strName)
+        {
+            foreach (string str1 in str)
+            {
+                if (str1.Contains(strName))
+                {
+                    return str1.Split('=')[1];
+                }
+            }
+            return "none";
+        }
+    }
+}
